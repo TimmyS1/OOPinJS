04f2205085176bb0b3b2946a44254b218522bc83
TimmyS1 <timothy.solokha@hilliardschools.org>
2017-10-24 13:39:11 -0400

    added some stuff

--- sketch.js
+++ sketch.js
@@ -5,22 +5,45 @@ var bubble = {
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas(600, 400); 
+	bubble1 = new Bubble(200,200, 40);
+	bubble2 = new Bubble(400,200, 20);
 }
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	move();
-	display();
+	bubble1.move();
+	bubble1.show();
+	bubble2.move();
+	bubble2.show();
 }
 
-function display() {
-	stroke(255); // white outline
-	strokeWeight(4); // line width
-	noFill();
-	ellipse(bubble.x, bubble.y, 24, 24); // draw an ellipse/circle
+class Bubble {
+	contructor(x, y, r){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	
+	move(){
+		this.x = this.x + random(-5,5);
+		this.y = this.y + random(-5,5);
+	}
+	show(){
+		stroke(255);
+		strokeWeight(4);
+		noFill();
+		ellipse(this.x, this.y, this.r*2);
+	}
 }
 
-function move() {
-	bubble.x = bubble.x + random(-5,5);
-	bubble.y = bubble.y + random(-5,5);
-}
+/*move(){
+		this.x = this.x + random(-5,5);
+		this.y = this.y + random(-5,5);
+	}
+	show(){
+		stroke(255);
+		strokeWeight(4);
+		noFill();
+		ellipse(this.x, this.y, 24, 24);
+	}
+	*/
